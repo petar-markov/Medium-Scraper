8fb4e123ff0f1104003b414c0d4201c824ed67f7
Petar Markov Tsvetkov <ptsvetkov01@gmail.com>
2020-12-15 13:00:45 +0200

    Add files via upload

---
+++ author.py
@@ -0,0 +1,9 @@
+class Article:
+    """
+    An object to pack all the info for an article.
+    Nice to have if we extend the logic.
+    """
+
+    def __init__(self, author, author_url:
+        self.author = author 
+        self.author_url = author_url

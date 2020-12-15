fe9edbb11fa8236fac34e5a9a698fc539be7339c
Petar Markov Tsvetkov <ptsvetkov01@gmail.com>
2020-12-15 13:00:28 +0200

    Update article.py

--- article.py
+++ article.py
@@ -4,7 +4,7 @@ class Article:
     Nice to have if we extend the logic.
     """
 
-    def __init__(self, author_id,          
+    def __init__(self, author_id, 
                 article_title, 
                 article_subtitle, 
                 article_url, 
@@ -16,15 +16,15 @@ class Article:
                 section_titles, 
                 paragraphs_num, 
                 paragraphs):
-        self.author = author_id, 
-        self.article_title = article_title, 
-        self.article_subtitle = article_subtitle, 
-        self.article_url = article_url, 
-        self.article_claps = article_claps, 
-        self.read_time = read_time, 
-        self.responses = responses, 
-        self.date_created = date_created, 
-        self.sections_num = sections_num, 
-        self.section_titles =section_titles, 
-        self.paragraphs_num = paragraphs_num, 
+        self.author_id = author_id
+        self.article_title = article_title 
+        self.article_subtitle = article_subtitle 
+        self.article_url = article_url 
+        self.article_claps = article_claps
+        self.read_time = read_time 
+        self.responses = responses
+        self.date_created = date_created
+        self.sections_num = sections_num
+        self.section_titles =section_titles
+        self.paragraphs_num = paragraphs_num
         self.paragraphs = paragraphs

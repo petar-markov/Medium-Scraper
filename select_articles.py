a783d2d665aed342a159a7a72615b875a640e8cf
Petar Markov Tsvetkov <ptsvetkov01@gmail.com>
2020-12-15 12:33:46 +0200

    Update select_articles.py

--- select_articles.py
+++ select_articles.py
@@ -3,9 +3,15 @@ import sqlite3
 conn = sqlite3.connect('medium_articles.db')
 
 c = conn.cursor()
+c.execute("SELECT * FROM medium_authors")
 
-c.execute("SELECT DISTINCT article_title, author, date_created, read_time, article_claps, responses, article_url FROM articles")
+print("Distinct Authors")
+for row in  c.fetchall():
+    print(row)
 
+c.execute("SELECT article_id, author_id, article_title, article_subtitle, article_url, article_claps, read_time, responses, date_created FROM articles")
+
+print("Articles")
 for row in c.fetchall():
     print(row)
 
